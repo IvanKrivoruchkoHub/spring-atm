753590464ca2a486db67838a892ccda34e628f14
ivan <Ivan.krivoruchko.1999@gmail.com>
2020-04-16 22:46:33 +0300

    first commit

---
+++ src/main/java/com/example/springatm/SpringatmApplication.java
@@ -0,0 +1,13 @@
+package com.example.springatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringatmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringatmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springatm/SpringatmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springatm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringatmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
